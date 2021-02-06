1e97249c96a4412eaa81df24691f29c4fddc124c
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-06 11:38:57 -0600

    got inputs in client js, set buttons and values in client js

--- public/client.js
+++ public/client.js
@@ -3,5 +3,37 @@ console.log('client js up and running sir');
 $(document).ready(onReady);
 
 function onReady() {
-  console.log('Jquery reporting for duty sir');
+  $('.operator-btns').on('click', setOperator);
+  $('#equals').on('click', getAnswer);
+  $('#clear').on('click', clearInputs);
+}
+
+// global variables
+let operator = '';
+let num1 = 0;
+let num2 = 0;
+
+// functions
+
+function clearInputs() {
+  $('#first-numb-input').val('');
+  $('#second-numb-input').val('');
+  operator = '';
+  num1 = 0;
+  num2 = 0;
+}
+
+function setOperator(e) {
+  operator = $(e.target).attr('id');
+  // console.log(operator);
+}
+
+function getAnswer() {
+  gatherInputs();
+  console.log(`we are going to ${operator} the numbers ${num1} and ${num2}`);
+}
+
+function gatherInputs() {
+  num1 = $('#first-numb-input').val();
+  num2 = $('#second-numb-input').val();
 }
